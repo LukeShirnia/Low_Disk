ac8a46862cde14aac8463d0ae445f6655511c085
LukeShirnia <root@new.lukeslinux.co.uk>
2018-06-20 08:43:09 +0100

    Script update

--- disk_usage_check.sh
+++ disk_usage_check.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-lsof_check() {
+BREAK="============================================================"
 
-    echo -ne "\n $BREAK \n \t Number of Open Deleted Files on:$filesystem \n $BREAK \n\n";
-    lsof 2> /dev/null | grep $filesystem | grep deleted | wc -l;
- 
-    echo -ne "\n $BREAK \n \t Open Deleted Files on :$filesystem bigger than 1GB \n $BREAK \n\n";
-    lsof 2> /dev/null | grep $filesystem | grep deleted| awk '{ if($9 > 1048576) print $9/1048576, "MB ",$9,$1 }' | sort -n -u | tail;
-
-}
+NotRun=''
 
 ServerTime() {
     date "+%F %H:%M %Z"
@@ -16,31 +10,66 @@ ServerTime() {
 PrintHeader() {
     echo -ne "\n $BREAK \n \t == $1 == \n $BREAK \n\n";
 }
-Intro() {
-    echo
-    echo "If you would like to run this against another filesystem please specify it with the -f flag"
-    echo "Example: monkey -d -- -f /data/"
-    echo ""
+usage() {
+    echo "Usage : $0"
+    echo "Usage : $0 -f filesystem"
+    exit 1
+}
+lsof_check_number() {
+    PrintHeader "Number of Open Deleted Files on: $filesystem"
+    if [ $(lsof 2> /dev/null | grep $filesystem | grep deleted | wc -l ) ]; then
+        lsof 2> /dev/null | grep $filesystem | grep deleted | wc -l;
+    else
+        return
+    fi
+    
+    PrintHeader "Open Deleted Files Over 1GB"
+    if [ $(lsof 2> /dev/null | awk '/$filesystem/ && /deleted/' | awk '{ if($9 > 1048576) print $9/1048576, "MB ",$9,$1 }' | sort -n -u | tail ) ]; then
+        echo -ne "\n $BREAK \n \t Open Deleted Files on :$filesystem bigger than 1GB \n $BREAK \n\n";
+        lsof 2> /dev/null | awk '/$filesystem/ && /deleted/' | awk '{ if($9 > 1048576) print $9/1048576, "MB ",$9,$1 }' | sort -n -u | tail;
+    else
+        echo "No deleted files over 1GB"
+        echo
+    fi
 }
 
-BREAK="============================================================"
 
 
+case $1 in
 
-Intro
+"" )
+    filesystem="/"   
+;;
 
-# check if alternative filesystem has been specified
-if [ $# == 0 ]; then
-    filesystem="/"
-elif [ $# == 2 ] && [ $1 == '-f' ]; then
-    filesystem=$2
-fi
+"-f")
+    case $# in
 
-# Check filesystem exists
-if [ ! -d $filesystem ]; then
-    echo "Filesystem does NOT exist"
-    exit
-fi
+    "1")
+        echo "Please enter 2 arguments"
+        echo
+        usage
+    ;;
+    "2")
+        if [ -d $2 ]; then
+            filesystem=$2
+        else
+            echo "Filesystem argument doesn't exist"
+            echo
+            usage
+        fi
+    ;;
+    * ) 
+        usage    
+    ;;
+    esac
+;;
+
+"--help" | "-h" | *)
+    usage
+;;
+
+esac
+
 
 PrintHeader "Server Date/Time"
 
@@ -65,14 +94,18 @@ find $filesystem -mount -ignore_readdir_race -type f -exec du {} + 2>&1 | sort -
 PrintHeader "Largest Files Older Than 30 Days"
 find $filesystem -mount -ignore_readdir_race -type f -mtime +30 -exec du {} + 2>&1 | sort -rnk1,1 | head -20 | awk 'BEGIN{ CONVFMT="%.2f";}{ $1=( $1 / 1024 )"M"; print; }' | column -t
 
-PrintHeader "Volume Group Usage"
 
-vgs $(df -h $filesystem | grep dev | awk '{print $1}'| cut -d\- -f1| cut -d\/ -f4)
-
-
-if [ $( which losf 2>/dev/null ) ]; then 
-    lsof_check
+if [ $( vgs $(df -h $filesystem | grep dev | awk '{print $1}'| cut -d\- -f1| cut -d\/ -f4) ) ]; then
+    PrintHeader "Volume Group Usage"
+    vgs $(df -h $filesystem | grep dev | awk '{print $1}'| cut -d\- -f1| cut -d\/ -f4)
 fi
+
+
+if [ $( which lsof 2>/dev/null ) ]; then 
+    lsof_check_number
+fi
+
+exit 0
 
 echo 
 echo $BREAK
